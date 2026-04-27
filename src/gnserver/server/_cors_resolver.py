23097b2b6141b0243e6eeea3b376f66f399ba3b7
KeyisB <keyisbp@gmail.com>
2026-04-27 13:55:05 +0300

    Bump version to 0.0.196 in pyproject.toml; add object type validation in CORS resolver for enhanced security.

--- src/gnserver/server/_cors_resolver.py
+++ src/gnserver/server/_cors_resolver.py
@@ -140,6 +140,9 @@ def resolve_cors(request: GNRequest, cors: CORSObject | None) -> None:
     if request.client.type not in cors.allow_client_types and request.client.type_int != 0:
         raise AllGNFastCommands.cors.ClientTypeNotAllowed({'message': f'Client type {request.client.type} not allowed for CORS. Allowed {cors.allow_client_types}'})
     
+    if request.object.type not in cors.allow_object_types and request.client.type_int != 0:
+        raise AllGNFastCommands.cors.ObjectTypeNotAllowed({'message': f'Object type {request.object.type} not allowed for CORS. Allowed {cors.allow_object_types}'})
+
     if cors.allow_origins is not None:
         if request._origin is None:
             raise AllGNFastCommands.cors.OriginNotAllowed({'message': 'Route has cors but request has no origin url'})
